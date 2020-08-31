ad8504703a73e6484bfc5bad15179fc4458916e5
sunjulei <1374856829@qq.com>
2020-09-01 00:20:16 +0800

    Code14_Simple_最长公共前缀_一种解法

---
+++ src/sunlee/com/Code14_Simple.java
@@ -0,0 +1,50 @@
+package sunlee.com;
+
+/**
+ * 14. 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * <p>
+ * 示例 2:
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * <p>
+ * 说明:
+ * 所有输入只包含小写字母 a-z 。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+ *
+ * @author sunlee
+ * @date 2020/8/31 22:44
+ */
+public class Code14_Simple {
+
+    public static void main(String[] args) {
+        String str[] = new String[]{"flosw", "flos", "floswm"};
+        System.out.println(longestCommonPrefix(str));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+}

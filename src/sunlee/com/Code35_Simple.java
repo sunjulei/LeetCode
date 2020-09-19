f42853c10dfabef948a555e1eab12b233021093f
sunjulei <1374856829@qq.com>
2020-09-19 09:52:25 +0800

    Code35_Simple_搜索插入位置

---
+++ src/sunlee/com/Code35_Simple.java
@@ -0,0 +1,61 @@
+package sunlee.com;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 你可以假设数组中无重复元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * <p>
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * <p>
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * <p>
+ * <p>
+ * 示例 4:
+ * <p>
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * <p>
+ * Related Topics 数组 二分查找
+ * 👍 689 👎 0
+ *
+ * @author sunlee
+ * @date 2020/9/19 9:39
+ */
+public class Code35_Simple {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 6};
+        System.out.println(searchInsert(arr, 4));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
